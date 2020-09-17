4eaebc652839b8f99402dc6616274d091e40d5f8
castaneai <castaneai@castaneai.net>
2020-09-17 23:25:30 +0900

    add bgm

--- main.cpp
+++ main.cpp
@@ -4,12 +4,31 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 {
 	SetOutApplicationLogValidFlag(FALSE);
 	ChangeWindowMode(TRUE);
+	SetGraphMode(640, 480, 32, 30);
 	if (DxLib_Init() == -1)
 	{
 		return -1;
 	}
-	DrawPixel(320, 240, GetColor(255, 255, 255));
-	WaitKey();
+	SetDrawScreen(DX_SCREEN_BACK);
+	SetMainWindowText("microgame");
+
+	int bgm = LoadSoundMem("BGM/hohoemi.mp3");
+	if (bgm == -1)
+	{
+		MessageBox(NULL, "failed to load BGM/hohoemi.mp3", "Error", MB_OK);
+	}
+	PlaySoundMem(bgm, DX_PLAYTYPE_LOOP, TRUE);
+
+	char i = 0;
+	while (true)
+	{
+		if (ScreenFlip() == -1) break;
+		if (ClearDrawScreen() == -1) break;
+		if (ProcessMessage() == -1) break;
+
+		DrawBox(0, 0, 300, 300, GetColor(i, i, i), TRUE);
+		i++;
+	}
 	DxLib_End();
 	return 0;
 }
